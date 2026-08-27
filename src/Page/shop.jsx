5a7ea694fe4d4a2f12db3e769ff1881ed3b4d991
Ombeer-singh <sagars9528@gmail.com>
2026-08-27 22:17:21 +0530

    Fix Popular component import

--- src/Page/shop.jsx
+++ src/Page/shop.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Hero from '../components/Hero/Hero';
-import Popular from '../components/Popular/popular';
+import Popular from '../components/Popular/Popular';
 import Offers from "../components/Offers/Offers"
 import Newcollections from '../components/Newcollections/Newcollections';
 import NewLetter from '../components/Newletter/Newletter';
